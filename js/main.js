d02f6d6fc7ccc139ad8fe3ef590bc2482b91e0c1
sandrofalves <sandrofalves@hotmail.com>
2021-06-30 17:18:33 -0300

    Fim do exemplo

---
+++ js/main.js
@@ -0,0 +1,17 @@
+/*
+alert("Meu JS");
+var list= ["laranja", "pera", "uva"];
+var frutas =[{nome : "maca", cor: "verde" }, {nome : "uva", cor: "roxa" }]
+list.push("goiaba");
+console.log(list);
+console.log(frutas);
+console.log(frutas[1].nome)
+
+*/
+function ClickForm(){
+    alert("Obrigado por Clicar!");
+}
+
+function Redirecionar(){
+    window.open("http://www.uol.com.br");
+}
